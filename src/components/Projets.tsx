35766e313a7e4aeeb812e72575a116f8056d9ad9
GalsenBoy <vipandco3@gmail.com>
2023-10-26 10:57:13 +0200

    Ajout de card project + optimisation sideBar

--- src/components/Projets.tsx
+++ src/components/Projets.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import { NavLink } from "react-router-dom";
 import "../css/projets.scss";
 
 export default function Projets() {
   const projects = [
-    { name: "/capcv.png", valeur: "CURRICULUM VITAE" },
-    { name: "/clone_spotify.png", valeur: "CLONE SPOTIFY" },
-    { name: "/retrogems.png", valeur: "SITE RETROGEMS" },
-    { name: "/waounde.png", valeur: "ASSOCIATION COMMUNE DE WAOUNDE" },
+    { name: "/capcv.png", valeur: "CURRICULUM VITAE",to:"https://cvbilaly.netlify.app/" },
+    { name: "/clone_spotify.png", valeur: "CLONE SPOTIFY",to:"https://github.com/GalsenBoy/clone-spotify_react_ts" },
+    { name: "/retrogems.png", valeur: "SITE RETROGEMS",to:"https://retrogems.fr/"  },
+    { name: "/waounde.png", valeur: "ASSOCIATION COMMUNE DE WAOUNDE",to:"https://galsenboy.netlify.app"  },
   ];
 
   return (
     <div id="projet-content">
       {projects.map((project, index) => (
         <div className="project" key={index}>
-          <img src={project.name} alt={project.valeur} />
-          <h4>{project.valeur}</h4>
+          <NavLink to={project.to}>
+            <img src={project.name} alt={project.valeur} />
+            <h4>{project.valeur}</h4>
+          </NavLink>
         </div>
       ))}
     </div>
