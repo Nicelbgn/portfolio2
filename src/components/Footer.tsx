97290a0ed942e4e64926cb753af8063f73091c60
GalsenBoy <vipandco3@gmail.com>
2023-10-27 15:17:59 +0200

    Ajout de footer + optimisation affichage

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    const year = new Date().getFullYear()
+    return (
+        <footer>
+            <h2>{year} © BILALY CISSOKHO</h2>
+        </footer>
+    )
+}
