44504a5bb5a9faa271982c1b3ec97ecb07699b3f
GalsenBoy <vipandco3@gmail.com>
2023-11-02 11:56:12 +0100

    suppression du dark-mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,12 @@
 import Content from "../components/Content";
 import SideBar from "../components/SideBar";
-import { useState } from "react";
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
   return (
     <section>
-      <div
-        className={isDarkMode ? "dark-mode" : "light-mode"}
-        id="home-content"
-      >
+      <div id="home-content">
         <SideBar />
-        <div>
-        <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
-          <Content />
-        </div>
+        <Content />
       </div>
     </section>
   );
