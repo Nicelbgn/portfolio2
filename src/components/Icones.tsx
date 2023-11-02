0d543eb7fb1b43fd671fb3124ad392c86d5154e0
GalsenBoy <vipandco3@gmail.com>
2023-11-02 16:33:23 +0100

    Clean code

---
+++ src/components/Icones.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import "../css/sideBar.scss";
+export default function Icones() {
+  const icons = [
+    {
+      to: " https://www.linkedin.com/in/bilaly-cissokho-473400278?originalSubdomain=fr",
+      src: "/linkedin.png",
+    },
+    {
+      to: " https://github.com/GalsenBoy?tab=repositories",
+      src: "/git.png",
+    },
+    {
+      to: "https://cvbilaly.netlify.app/",
+      src: "/cv.png",
+    },
+  ];
+  return (
+    <div id="reseaux-items">
+      {icons.map((icon) => (
+        <NavLink to={icon.to}>
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            src={icon.src}
+            alt=""
+          />
+        </NavLink>
+      ))}
+    </div>
+  );
+}
