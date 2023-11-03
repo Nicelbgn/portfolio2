10a5e12db624466cca5df59e394564e4e462e0d0
GalsenBoy <vipandco3@gmail.com>
2023-11-03 11:45:30 +0100

    animation burger done

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -3,12 +3,15 @@ import { motion } from "framer-motion";
 import { generateAnimation } from "../utils/animations";
 import NavResponsive from "./NavResponsive";
 import { useState } from "react";
+import Hamburger from "hamburger-react";
 export default function Burger() {
   const [cliqued, setCliqued] = useState<boolean>(false);
+  const [isOpen, setOpen] = useState<boolean>(false)
 
   const handleClick = () => {
     setCliqued(!cliqued);
   };
+
   return (
     <motion.div
       initial="hidden"
@@ -16,11 +19,8 @@ export default function Burger() {
       variants={generateAnimation(0.25, "y")}
       id="burger-content"
     >
-      <div id="burger" onClick={handleClick}>
-        <input type="checkbox" id="check" />
-        <span id="span1"></span>
-        <span id="span2"></span>
-        <span id="span3"></span>
+      <div onClick={handleClick}>
+        <Hamburger toggled={isOpen} toggle={setOpen} direction="right" rounded/>
       </div>
       <div className={!cliqued ? "hidden" : "nav-responsive"}>
         {" "}
