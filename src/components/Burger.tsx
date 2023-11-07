9ee7a98758b5b9f16c44a09baaa9c9b6a7396e39
bilaly <billycsk@gmail.com>
2023-11-07 21:45:50 +0100

    Résolution problème des liens des projets

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 import { generateAnimation } from "../utils/animations";
 import NavResponsive from "./NavResponsive";
 import { useState } from "react";
-import { Divide as Hamburger } from 'hamburger-react'
+import { Divide as Hamburger } from "hamburger-react";
 export default function Burger() {
   const [cliqued, setCliqued] = useState<boolean>(false);
-  const [isOpen, setOpen] = useState<boolean>(false)
+  const [isOpen, setOpen] = useState<boolean>(false);
 
   const handleClick = () => {
     setCliqued(!cliqued);
@@ -20,7 +20,12 @@ export default function Burger() {
       id="burger-content"
     >
       <div id="burger" onClick={handleClick}>
-        <Hamburger toggled={isOpen} toggle={setOpen} direction="right" rounded/>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          direction="right"
+          rounded
+        />
       </div>
       <div className={!cliqued ? "hidden" : "nav-responsive"}>
         {" "}
