0c0d545bcccd36ef194c161fc4d6dbfa4e1c64f4
GalsenBoy <vipandco3@gmail.com>
2023-12-22 17:07:32 +0100

    Ajout projet blog

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -1,37 +1,37 @@
 export const projects = [
   {
     id: 1,
-    name: "/capcv.webp",
-    valeur: "CURRICULUM VITAE",
-    to: "https://bilaly.vercel.app/",
-    alt: "cv"
+    name: "/retrogems.webp",
+    valeur: "SITE RETROGEMS",
+    to: "https://retrogems.fr/",
+    alt: "retrogems",
   },
   {
     id: 2,
     name: "/spotify.webp",
     valeur: "CLONE SPOTIFY",
     to: "https://clone-spotify-react-ts.vercel.app/",
-    alt: "spotify"
+    alt: "spotify",
   },
   {
     id: 3,
-    name: "/retrogems.webp",
-    valeur: "SITE RETROGEMS",
-    to: "https://retrogems.fr/",
-    alt: "retrogems"
+    name: "/blog.webp",
+    valeur: "MON BLOG",
+    to: "https://bilaly-blog.vercel.app/",
+    alt: "bilalyblog",
   },
   {
     id: 4,
     name: "/waounde.webp",
     valeur: "ASSOCIATION COMMUNE DE WAOUNDE",
     to: "https://galsenboy.netlify.app",
-    alt: "waounde"
+    alt: "waounde",
   },
   {
     id: 5,
     name: "/github.webp",
     valeur: "VOIR D'AUTRES PROJECTS",
     to: "https://github.com/GalsenBoy?tab=repositories",
-    alt: "github"
+    alt: "github",
   },
 ];
