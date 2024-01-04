2ae357b2bfdad0910480ae5c43cdd2605bd80f0f
GalsenBoy <vipandco3@gmail.com>
2024-01-04 12:17:18 +0100

    Ajout de blog image

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -1,34 +1,33 @@
 export const projects = [
   {
-    id: 1,
     name: "/retrogems.webp",
     valeur: "SITE RETROGEMS",
     to: "https://retrogems.fr/",
     alt: "retrogems",
   },
   {
-    id: 2,
+   
     name: "/spotify.webp",
     valeur: "CLONE SPOTIFY",
     to: "https://clone-spotify-react-ts.vercel.app/",
     alt: "spotify",
   },
   {
-    id: 3,
+  
     name: "/blog.webp",
     valeur: "MON BLOG",
     to: "https://bilaly-blog.vercel.app/",
     alt: "bilalyblog",
   },
   {
-    id: 4,
+  
     name: "/waounde.webp",
     valeur: "ASSOCIATION COMMUNE DE WAOUNDE",
     to: "https://galsenboy.netlify.app",
     alt: "waounde",
   },
   {
-    id: 5,
+
     name: "/github.webp",
     valeur: "VOIR D'AUTRES PROJECTS",
     to: "https://github.com/GalsenBoy?tab=repositories",
